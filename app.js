6005560b957443bc4cd281087689b8f483d97866
DParkinson-BYUI <parkidr@gmail.com>
2021-09-25 21:16:22 -0700

    Completed Prove02 videos

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
+
+const app = express();
+
+// Route setup. You can implement more in the future!
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .use(bodyParser({ extended: false })) // For parsing the body of a POST
+  .use('/admin', adminData.routes)
+  .use(shopRoutes)
+  .use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  })  
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));  
---
+++ routes/admin.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title, price: req.body.price, description: req.body.description });
+  res.redirect('/');
+});
+
+// /admin/remove-product => POST
+router.post('/remove-product', (req, res, next) => {
+  products.splice(req.body.deleteBook, 1)
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;

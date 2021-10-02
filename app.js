fd4925d0947a708e12b2d1341f04facbb08422f9
DParkinson-BYUI <parkidr@gmail.com>
2021-10-02 15:54:33 -0700

    Push Week3 Prove

--- app.js
+++ app.js
@@ -1,24 +1,22 @@
-
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
 
+const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
+const errorController = require('./controllers/error');
 const app = express();
 
 // Route setup. You can implement more in the future!
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop');
 
 app
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
+  .set('views', 'views')
   .use(bodyParser({ extended: false })) // For parsing the body of a POST
-  .use('/admin', adminData.routes)
+  .use('/admin', adminRoutes)
   .use(shopRoutes)
-  .use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
-  })  
+  .use(errorController.get404)
   .listen(PORT, () => console.log(`Listening on ${PORT}`));  
--- routes/admin.js
+++ routes/admin.js
@@ -2,34 +2,26 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-const products = [];
+
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
-});
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title, price: req.body.price, description: req.body.description });
-  res.redirect('/');
-});
+router.post('/add-product', adminController.postAddProduct);
 
-// /admin/remove-product => POST
-router.post('/remove-product', (req, res, next) => {
-  products.splice(req.body.deleteBook, 1)
-  res.redirect('/');
-});
+// /admin/add-product => GET
+router.get('/edit-product:productId', adminController.getEditProduct);
 
-exports.routes = router;
-exports.products = products;
+router.post('/edit-product', adminController.postEditProduct);
+
+router.post('/delete-product', adminController.postDeleteProduct);
+
+module.exports = router;

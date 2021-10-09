ff8bd179e116770b45366929d807e90b5380d4a4
DParkinson-BYUI <parkidr@gmail.com>
2021-10-08 20:38:15 -0700

    prove04 initial commit

--- app.js
+++ app.js
@@ -1,22 +1,75 @@
 const path = require('path');
+const keys = require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000; // So we can run on heroku || (OR) localhost:5000
+const cors = require('cors');
+const corsOptions = {
+  origin: "dparki2-cse341ecommerce.herokuapp.com/",
+  optionsSuccessStatus: 200
+};
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const User = require('./models/user');
+
 const app = express();
 
-// Route setup. You can implement more in the future!
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const shopRoutes = require('./routes/shop');
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('view engine', 'ejs')
-  .set('views', 'views')
-  .use(bodyParser({ extended: false })) // For parsing the body of a POST
-  .use('/admin', adminRoutes)
-  .use(shopRoutes)
-  .use(errorController.get404)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));  
+// const optons = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   userCreateIndex: true,
+//   useFindAndModify: false,
+//   family: 4
+// };
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(async (req, res, next) => {
+  try{
+    const user = await User.findById('6160a8c1d4db714ef99ae0bf');
+    req.user = user;
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+ });
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(cors(corsOptions));
+
+app.use(errorController.get404);
+
+async function init() {
+  try {
+    await mongoose.connect(MONGODB_URL);
+    const user = await User.findOne();
+    if (!user) {
+      const user = new User({
+        name: 'Drew',
+        email: 'dparki2@yahoo.com',
+        cart: {
+          items: [],
+        },
+      });
+      await user.save();
+    }
+    app.listen(process.env.PORT || 3000);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+init();
+

c6c97aac86bd3a526b6c31dd61a89c00f4615bda
DParkinson-BYUI <parkidr@gmail.com>
2021-10-23 14:16:40 -0700

    prove06 initial push

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
@@ -30,12 +27,14 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
-const optons = {
+const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
   //userCreateIndex: true,
@@ -58,6 +57,12 @@ app.use(
 app.use(csrfProtection);
 app.use(flash());
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use(async (req, res, next) => {
   try{
     if (!req.session.user) {
@@ -68,21 +73,24 @@ app.use(async (req, res, next) => {
     next();
   } catch (err) {
     console.log(err);
+    next(new Error(err));
   }
  });
-
-app.use((req, res, next) => {
-  res.locals.isAuthenticated = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
-
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 app.use(cors(corsOptions));
 
+app.get('/500', errorController.get500);
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 async function init() {
   try {
